7f3aa89017a82a32c6bc6c28fa93a2f6ad41c2cb
Ricardo Valente <valentemesmo@gmail.com>
2016-05-11 00:56:53 -0300

    Chat test on android
    close #1

--- GameClient/Assets/ConnectToServer.cs
+++ GameClient/Assets/ConnectToServer.cs
@@ -1,75 +1,28 @@
-﻿using ClientSide;
+﻿using NetworkStuff;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ConnectToServer : MonoBehaviour
 {
-    public string Ip = "192.168.0.8";
-    public int Port = 8001;
-    private GameClient game;
-    private Dictionary<string, Thing> things = new Dictionary<string, Thing>();
-
-    public GameObject PlayerPrefab;
-    public GameObject BlockPrefab;
+    public string Ip = "192.168.0.2";
+    public int Port = 20010;
 
     void Start()
     {
-        game = new GameClient(SomethingChanged);
-        game.Connect(Ip,Port);                        
+        var client = Factory.CreateClient(8001,8002);
+        client.InformYourListeningPortToHost(Ip,Port,MessageReceived);
+        //client.SendMessage("Olá, mundo!");
+        
+    }
+    string Mensagem = "";
+    private void MessageReceived(string msg, Address address)
+    {
+        Debug.Log(msg);
+        Mensagem = msg;
     }
 
-    void SomethingChanged(Thing thingThatChanged)
-    {
-        Debug.Log("OPA!");
-        if (things.ContainsKey(thingThatChanged.Id) == false)
-        {
-            things[thingThatChanged.Id] = thingThatChanged;
-            adasdsa(thingThatChanged);
-        }
-    }
-
-    void OnDestroy()
-    {
-        game.Dispose();
-    }
-
-    void adasdsa(Thing newThing )
-    {
-        if (newThing is Player)
-        {
-            var go = Instantiate(PlayerPrefab);
-            var comp = go.AddComponent<PositionComponent>();
-            comp.Get_X = () => newThing.X.GetValue();
-            comp.Get_Y = () => newThing.Y.GetValue();
-
-            var anim = go.AddComponent<AnimatorComponent>();
-            anim.GetTriggerName = () =>
-            {
-                if ((newThing as Player).IsTouchingTheGround() == false)
-                {
-                    return "MidAir";
-                }
-                //TODO: use speed! not velocity
-                else if (newThing.Velocity_X.GetValue() > 0 || newThing.Velocity_X.GetValue() < 0)
-                {
-                    return "Walk";
-                }
-
-                return "Iddle";
-            };
-
-            go.GetComponent<FlipSprite>().ShouldFlipSprite = () =>
-            {
-                //TODO: change! use speed... not velocity
-                return newThing.Velocity_X.GetValue() < 0 /*&& (newThing as Player).IsTouchingTheGround()*/;
-            };
-        }
-        else if (newThing is Block)
-        {
-            var go = Instantiate(BlockPrefab);
-            var comp = go.AddComponent<PositionComponent>();
-            comp.Get_X = () => newThing.X.GetValue();
-            comp.Get_Y = () => newThing.Y.GetValue();
-        }
+    void OnGUI() {
+        GUILayout.Label(Mensagem);
     }
 }
