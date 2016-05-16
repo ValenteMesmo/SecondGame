a3eb0696cac5bf1db6318849ebbcff6c90fa936e
Ricardo Valente <valentemesmo@gmail.com>
2016-05-16 01:23:55 -0300

    Creating host and client scenes
    #1

---
+++ GameClient/Assets/StartHostingOnTouch.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnitySolution.InputComponents;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(DetectTouchOnThisGameObject))]
+public class StartHostingOnTouch : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<DetectTouchOnThisGameObject>().OnEnd += TouchDetector_OnEnd;
+    }
+
+    private void TouchDetector_OnEnd(object sender, PointEventArgs e)
+    {
+        SceneManager.LoadScene("ChatAsHost");
+    }
+}
--- GameCore/ClientExemple/Program.cs
+++ GameCore/ClientExemple/Program.cs
@@ -1,5 +1,7 @@
 ﻿using NetworkStuff;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace ClientExemple
 {
@@ -38,5 +40,32 @@ namespace ClientExemple
                 string.Format("{0}:{1} => {2}", arg2.Ip, arg2.Port, arg1)
                 );
         }
+    }
+}
+
+class Exemple : IExemple { }
+interface IExemple { }
+
+class Container
+{
+    private readonly Dictionary<Type, Func<object>> dictionary =
+        new Dictionary<Type, Func<object>>();
+
+    public void Register<T>(Func<T> getInstance) where T : class
+    {
+        var type = typeof(T);
+        if (dictionary.ContainsKey(type))
+            throw new Exception("!");
+
+        dictionary.Add(type, getInstance);
+    }
+
+    public T Resolve<T>()
+    {
+        var type = typeof(T);
+        if (dictionary.ContainsKey(type))
+            return (T)dictionary[type]();
+
+        throw new Exception("!");
     }
 }
