793a592a52e77c6259edd34185d604d4011a348b
Ricardo Valente <valentemesmo@gmail.com>
2016-05-24 00:04:15 -0300

    Foreach combination

--- GameCore/GameCore.Tests/UnitTest1.cs
+++ GameCore/GameCore.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace GameCore.Tests
 {
@@ -16,6 +17,28 @@ namespace GameCore.Tests
             var actual = sut.IsColliding(first, second);
 
             Assert.IsTrue(actual, "Collision not detected!");
+        }
+
+        [TestMethod]
+        public void ForEachCombination()
+        {
+            var sut = new List<string> {
+                "A", "B", "C", "D"
+            };
+
+            var actual = new List<string>();
+            sut.ForEachCombinations((first, second) =>
+            {
+                actual.Add(first + second);
+            });
+
+            Assert.AreEqual(6, actual.Count);
+            Assert.AreEqual("AD", actual[0]);
+            Assert.AreEqual("AC", actual[1]);
+            Assert.AreEqual("AB", actual[2]);
+            Assert.AreEqual("BD", actual[3]);
+            Assert.AreEqual("BC", actual[4]);
+            Assert.AreEqual("CD", actual[5]);
         }
     }
 }
---
+++ GameCore/GameCore/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameCore
+{
+    public static class ListExtensions
+    {
+        public static void ForEachCombinations<T>(this List<T> list, Action<T, T> action)
+        {
+            for (int i = 0; i < list.Count -1; i++)
+            {
+                for (int j = list.Count -1; j > i; j--)
+                {
+                    action(list[i], list[j]);
+                }
+            }
+        }
+    }
+}
