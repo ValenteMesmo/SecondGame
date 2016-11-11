4e44ccda01834d2da12c9a24fd7bc9877226201d
Ricardo Valente <valentemesmo@gmail.com>
2016-11-11 06:28:21 -0200

    Fixing invisible monster BUG

--- GameClient/Assets/MonsterScript.cs
+++ GameClient/Assets/MonsterScript.cs
@@ -9,20 +9,16 @@ public class MonsterScript : MonoBehaviour
 
     void Start()
     {
+        WorldComponent.Sandbox.MonsterCreated.Subscribe(MonsterCreated);
         WorldComponent.Sandbox
             .AddMonster.Publish(
             new Position(transform.position.x, transform.position.y));
-        WorldComponent.Sandbox.MonsterCreated.Subscribe(MonsterCreated);
     }
 
     private void MonsterCreated(Monster monster)
     {
-        Debug.Log(0);
-
         if (colliderJustToVisualize == null)
         {
-            Debug.Log(1);
-
             colliderJustToVisualize = gameObject.AddComponent<BoxCollider2D>();
             colliderJustToVisualize.size = new Vector2(
                 monster.Collider.Width,
