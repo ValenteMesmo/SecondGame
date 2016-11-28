0b925c784d420f1e369f9304e7f41b5e47c39ddb
Ricardo Valente <valentemesmo@gmail.com>
2016-11-28 00:59:24 -0200

    Changing remoteplayerfactory to alow multiple guests

--- GameClient/Assets/Scripts/RemotePlayerFactory.cs
+++ GameClient/Assets/Scripts/RemotePlayerFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class RemotePlayerFactory : MonoBehaviour
@@ -6,37 +7,41 @@ public class RemotePlayerFactory : MonoBehaviour
     void Start()
     {
         WorldComponent.Sandbox.GuestPosiitonUpdate.Subscribe(UpdateGuest);
-        WorldComponent.Sandbox.HostPosiitonUpdate.Subscribe(UpdateHost);
-        WorldComponent.Sandbox.OtherPlayerEnteredAsTheGuest.Subscribe(NewGuest);
-        WorldComponent.Sandbox.OtherPlayerEnteredAsTheHost.Subscribe(NewGuest);
+        WorldComponent.Sandbox.HostPosiitonUpdate.Subscribe(UpdateGuest);    
     }
+        
+    private readonly Dictionary<string, Vector2> positions =
+        new Dictionary<string, Vector2>();
 
-    bool createPLayer = false;
-    private GameObject player;
-    private Vector2 newPosition = new Vector2(0, 0);
+    private readonly Dictionary<string, GameObject> players =
+        new Dictionary<string, GameObject>();
 
-    private void NewGuest()
-    {
-        createPLayer = true;
-    }
-
-    private void UpdateHost(Common.Collider obj)
-    {
-        newPosition.x = obj.X;
-        newPosition.y = obj.Y;
-    }
+    private readonly List<string> PlayersToCreate = new List<string>();
 
     private void UpdateGuest(Common.Collider obj)
     {
-        newPosition.x = obj.X;
-        newPosition.y = obj.Y;
+        if (positions.ContainsKey(obj.Name) == false)
+        {
+            positions[obj.Name] = new Vector2(0, 0);
+            PlayersToCreate.Add(obj.Name);
+        }
+        var pos = positions[obj.Name];
+        pos.x = obj.X;
+        pos.y = obj.Y;
+        positions[obj.Name] = pos;
     }
 
     void Update()
     {
-        if (createPLayer && player == null)
-            player = (GameObject)Instantiate(Resources.Load("Prefab/Other Player"));
-        if (player != null)
-            player.transform.position = newPosition;
+        foreach (var name in PlayersToCreate.ToList())
+        {
+            PlayersToCreate.Remove(name);
+            players[name] = (GameObject)Instantiate(Resources.Load("Prefab/Other Player"));
+        }
+
+        foreach (var name in positions.Keys.ToList())
+        {
+            players[name].transform.position = positions[name];
+        }
     }
 }
